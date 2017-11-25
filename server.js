4439899cfb6f38a1ab2a6a48c46ef2616140f1fe
drx <74012x147@gmail.com>
2017-11-25 18:52:19 +0100

    1.11

--- server.js
+++ server.js
@@ -7,22 +7,21 @@ let app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname+'/public'));
 
 app.use((req, res, next) => {
 	let now = new Date().toString();
 	let log = `${now}: ${req.method}, ${req.url}`;
 
 	console.log(log);
-	fs.appendFile('server.log', log + '\n', () => {
-		console.log('Log created.');
-	});
+	fs.appendFile('server.log', log + '\n');
 	next();
 });
 
 // app.use((req, res, next) => {
 // 	res.render('maintenance.hbs');
 // });
+
+app.use(express.static(__dirname+'/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear();
@@ -45,12 +44,13 @@ app.get('/about', (request, response) => {
 	});
 });
 
+// coś tam
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Unable to fulfill request'
 	});
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
